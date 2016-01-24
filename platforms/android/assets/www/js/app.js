6d204223b1b40105e9a5498b5073a4d82493bb49
Ankit Patil <anankitpatil@gmail.com>
2016-01-24 15:46:38 +0530

    login and register complete

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,10 +1,5 @@
 // Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova', 'ngStorage'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,63 +18,63 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-  .state('app', {
+  $stateProvider.state('app', {
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
     controller: 'AppCtrl'
   })
-  
+
+  .state('app.newTopics', {
+    url: '/new-topic',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/new-topic.html',
+        controller: 'newTopicsCtrl'
+      }
+    }
+  })
+
+  .state('app.listTopics', {
+    url: '/list-topics',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/list-topics.html',
+        controller: 'listTopicsCtrl'
+      }
+    }
+  })
+
   .state('app.map', {
     url: '/map',
-	views: {
-	  'menuContent': {
-	    templateUrl: 'templates/map.html',
-	    controller: 'MapCtrl'
-	  }
-    }
-  })
-
-  .state('app.search', {
-    url: '/search',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/map.html',
+        controller: 'MapCtrl'
       }
     }
   })
 
-  .state('app.browse', {
-    url: '/browse',
+  .state('app.login', {
+    url: '/login',
     views: {
       'menuContent': {
-        templateUrl: 'templates/browse.html'
-      }
-    }
-  })
-	
-  .state('app.playlists', {
-    url: '/playlists',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlists.html',
-        controller: 'PlaylistsCtrl'
+        templateUrl: 'login.html',
+        controller: 'LoginCtrl'
       }
     }
   })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
+  .state('app.register', {
+    url: '/register',
     views: {
       'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+        templateUrl: 'register.html',
+        controller: 'RegisterCtrl'
       }
     }
-  });
-  
+  })
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/login');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,76 +1,131 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
+.controller('AppCtrl', function($scope, $rootScope, $ionicModal, $timeout) {
   // Form data for the login modal
-  $scope.loginData = {};
+  $rootScope.loginData = {};
+	$rootScope.registerData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
+  $rootScope.newTopic = function() {
+    $state.go('app.newTopic');
   };
 
-  // Open the login modal
+  $rootScope.listTopics = function() {
+    $state.go('app.listTopics');
+  };
+
   $scope.login = function() {
     $scope.modal.show();
   };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+})
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+.controller('LoginCtrl', function($scope, $state, $location, $ionicModal, $ionicNavBarDelegate, $interval, $ionicPopup, $timeout, $ionicLoading, $stateParams, $ionicSideMenuDelegate, $rootScope, $http) {
+  $ionicSideMenuDelegate.canDragContent(false);
+
+  console.log("username" + window.localStorage['username']);
+  if (!window.localStorage['username']) {
+    $scope.doLogin = function() {
+      console.log('Doing login', $scope.loginData);
+      $scope.loginAjax();
+    };
+
+    $scope.loginAjax = function() {
+      $http({
+        method: 'GET',
+        url: 'http://146.185.173.106:4713/api/login/' + $scope.loginData.username + '/' + $scope.loginData.phone,
+      }).success(function(json) {
+        console.log(json);
+
+        if (json.username === $scope.loginData.username && json.phone === $scope.loginData.phone) {
+          window.localStorage['username'] = $scope.loginData.username;
+          window.localStorage['phone'] = $scope.loginData.phone;
+          $state.go('app.map');
+        } else {
+          $ionicLoading.show({
+            template: "Please enter correct username and phone.",
+            noBackdrop: true,
+            duration: 2000
+          });
+        }
+
+      }).error(function() {
+        console.log("Username ajax error");
+      });
+    };
+  } else {
+    $state.go('app.map');
+  }
+
+  $scope.register = function() {
+    $state.go('app.register');
+  }
+})
+
+.controller('RegisterCtrl', function($scope, $state, $location, $ionicModal, $ionicNavBarDelegate, $interval, $ionicPopup, $timeout, $ionicLoading, $stateParams, $ionicSideMenuDelegate, $rootScope, $http) {
+  $ionicSideMenuDelegate.canDragContent(false);
+
+  $scope.doregister = function() {
+    console.log('Doing register', $rootScope.registerData);
+
+    if (!$rootScope.registerData.username || !$rootScope.registerData.phone || !$rootScope.registerData.display) {
+      $ionicLoading.show({
+        template: "All fields are mandatory !",
+        noBackdrop: true,
+        duration: 2000
+      });
+
+    } else {
+      $scope.regAjax();
+    }
+  }
+
+  $scope.regAjax = function() {
+
+		console.log(JSON.stringify($rootScope.registerData));
+
+    $http({
+      method: 'POST',
+      url: 'http://146.185.173.106:4713/api/register',
+      data: $rootScope.registerData,
+      headers: {
+        "Accept": "text/json",
+				"Content-Type": "application/json"
+      }
+    }).success(function(json) {
+      console.log(json);
+    }).error(function() {
+      console.log("Username ajax error");
+    });
   };
 })
 
 .controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
-  var options = {timeout: 10000, enableHighAccuracy: true};
- 
-  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
- 
+  var options = {
+    timeout: 10000,
+    enableHighAccuracy: true
+  };
+
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
+
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
+    console.log("Current position" + latLng);
     var mapOptions = {
       center: latLng,
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
- 
+
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
- 
-  }, function(error){
+
+  }, function(error) {
     console.log("Could not get location");
   });
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('newTopicCtrl', function($scope, $state, $location, $ionicModal, $ionicNavBarDelegate, $interval, $ionicPopup, $timeout, $ionicLoading, $stateParams, $ionicSideMenuDelegate, $rootScope, $http) {
+
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('listTopicsCtrl', function($scope, $state, $location, $ionicModal, $ionicNavBarDelegate, $interval, $ionicPopup, $timeout, $ionicLoading, $stateParams, $ionicSideMenuDelegate, $rootScope, $http) {
+
 });
